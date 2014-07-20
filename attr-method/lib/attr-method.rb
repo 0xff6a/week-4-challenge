24354656c252944be5c2d4e8874789b9ec01c8fd
Jeremy Fox <foxjerem@gmail.com>
2014-07-20 12:33:57 +0100

    Added my own reader, writer and accessor methods

---
+++ attr-method/lib/attr-method.rb
@@ -0,0 +1,53 @@
+class Class
+
+	def my_attr_reader(*args)
+		args.each do |arg|
+			self.class_eval("def #{arg};@#{arg};end")
+		end
+	end
+
+	def my_attr_writer(*args)
+		args.each do |arg|
+			self.class_eval("def #{arg}=(val);@#{arg}=val;end")
+		end
+	end
+
+	def my_attr_accessor(*args)
+		my_attr_reader(*args)
+		my_attr_writer(*args)
+	end
+
+end
+
+class TestGetter
+
+	my_attr_reader :first_variable, :second_variable
+
+	def initialize
+		@first_variable = true
+		@second_variable = false
+	end
+
+end
+
+class TestWriter
+
+	my_attr_writer :first_variable, :second_variable
+
+	def initialize
+		@first_variable = true
+		@second_variable = false
+	end
+
+end
+
+class TestAccessor
+
+	my_attr_accessor :first_variable, :second_variable
+
+	def initialize
+		@first_variable = true
+		@second_variable = false
+	end
+
+end
---
+++ attr-method/spec/attr-method_spec.rb
@@ -0,0 +1,42 @@
+require 'attr-method'
+
+describe 'attr methods' do 
+
+	let(:test_read) 	{ TestGetter.new		}
+	let(:test_write) 	{ TestWriter.new		}
+	let(:test_access) { TestAccessor.new	}
+
+	context 'attr_reader' do
+
+		it 'should create a getter method' do
+			expect(test_read.first_variable).to be true
+			expect(test_read.second_variable).to be false
+		end
+
+	end
+
+	context 'attr_writer' do
+
+		it 'should create a setter method' do
+			test_write.first_variable = false
+			test_write.second_variable = true
+			expect(test_write.instance_variable_get("@first_variable")).to be false
+			expect(test_write.instance_variable_get("@second_variable")).to be true
+		end
+
+	end
+
+	context 'attr_accessor' do
+
+		it 'should create a getter method' do
+			expect(test_access.first_variable).to be true
+		end
+
+		it 'should create a setter method' do
+			test_access.first_variable = false
+			expect(test_access.first_variable).to be false
+		end
+
+	end
+	
+end
