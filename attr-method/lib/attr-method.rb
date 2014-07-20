cec8e5e282f46e1509d863d3f2e1aa00ee67209d
Jeremy Fox <foxjerem@gmail.com>
2014-07-20 13:15:08 +0100

    Added error handling for invalid argument to attr-method

--- attr-method/lib/attr-method.rb
+++ attr-method/lib/attr-method.rb
@@ -2,12 +2,14 @@ class Class
 
 	def my_attr_reader(*args)
 		args.each do |arg|
+			raise "argument must be a symbol" if not arg.is_a?(Symbol)
 			self.class_eval("def #{arg};@#{arg};end")
 		end
 	end
 
 	def my_attr_writer(*args)
 		args.each do |arg|
+			raise "argument must be a symbol" if not arg.is_a?(Symbol)
 			self.class_eval("def #{arg}=(val);@#{arg}=val;end")
 		end
 	end
--- attr-method/spec/attr-method_spec.rb
+++ attr-method/spec/attr-method_spec.rb
@@ -13,6 +13,11 @@ describe 'attr methods' do
 			expect(test_read.second_variable).to be false
 		end
 
+		it 'should take only symbols as arguments' do
+			expect { Class::my_attr_reader("jlj") }.to raise_error
+			expect { Class::my_attr_reader(:hi) }.not_to raise_error
+		end
+
 	end
 
 	context 'attr_writer' do
@@ -22,6 +27,11 @@ describe 'attr methods' do
 			test_write.second_variable = true
 			expect(test_write.instance_variable_get("@first_variable")).to be false
 			expect(test_write.instance_variable_get("@second_variable")).to be true
+		end
+
+		it 'should take only symbols as arguments' do
+			expect { Class::my_attr_writer("jlj") }.to raise_error
+			expect { Class::my_attr_writer(:hi) }.not_to raise_error
 		end
 
 	end
@@ -35,6 +45,11 @@ describe 'attr methods' do
 		it 'should create a setter method' do
 			test_access.second_variable = false
 			expect(test_access.second_variable).to be false
+		end
+
+		it 'should take only symbols as arguments' do
+			expect { Class::my_attr_accessor("jlj") }.to raise_error
+			expect { Class::my_attr_accessor(:hi) }.not_to raise_error
 		end
 
 	end
